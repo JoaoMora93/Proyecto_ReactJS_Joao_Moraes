e53e553684a45fbfc558fcab29cf8ae9df8501e8
Joao Moraes <joaomcarril@gmail.com>
2023-07-05 01:25:52 -0300

    comienzo de la app

---
+++ tienda_reactJS_ JoaoM/src/components/Header.jsx
@@ -0,0 +1,20 @@
+
+
+
+
+export const Header = () => {
+
+    return (
+        <header>
+            <div>
+                <h1>LOGO</h1>
+
+                <nav>
+                    <a href="#">Enlace 1</a>
+                    <a href="#">Enlace 2</a>
+                    <a href="#">Enlace 3</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
