0eb61b7c610377dac0926a7cabbff3d495637f41
Joao Moraes <joaomcarril@gmail.com>
2023-07-18 02:59:06 -0300

    Galeria mejorada productos con nombre

--- tienda_reactJS_ JoaoM/src/components/ItemCard/ItemCard.jsx
+++ tienda_reactJS_ JoaoM/src/components/ItemCard/ItemCard.jsx
@@ -4,25 +4,26 @@ import './ItemCard.scss'
 const ItemCard = ({item}) => {
 
     return (
-        <div className="Item__Card">
-            <div className="Item__img">
-                <img src={item.img} alt={item.nombre} />
-            </div>
+            <div className="Item__Card m-10">
+                <div className="Item__img">
+                    <img className="saturate-150" src={item.img} alt={item.nombre} />
+                </div>
 
-            <div className='Item__text'>
-            <h3>{item.nombre}</h3>
-            <p>{item.descripcion}</p>
-            </div>
+                <div className='Item__text'>
+                <h3>{item.nombre}</h3>
+                <p>{item.descripcion}</p>
+                </div>
 
-            <div className="Item__price">
-                <p >Precio: {item.precio}</p>
-            </div>
+                <div className="Item__price">
+                    <p >Precio: {item.precio}</p>
+                </div>
 
-            <div className="Item__btn">
-                <button>Ver más</button>     
-            </div>
+                <div className="Item__btn">
+                    <button>Ver más</button>     
+                </div>
 
-        </div>
+            </div>
+        
     )
 }
 
--- tienda_reactJS_ JoaoM/src/components/ItemList/ItemList.jsx
+++ tienda_reactJS_ JoaoM/src/components/ItemList/ItemList.jsx
@@ -6,15 +6,17 @@ const ItemList = ({productos}) => {
     
     return (
         <div>
-            <h1 className='item__title font-bold text-3xl'>Productos</h1>
+            <h1 className='item__title text-inherit font-bold text-3xl pt-8'>Productos</h1>
             <hr />
-            <p className='font-sans hover:bg-violet-300 px-5'>Emprendimiento de sahumadores artesanales y más artículos para la renovación de energía.</p>
-            <h2 className="text-xl px-5 font-bold">
+            <p className='ms-5 mt-10 mb-0 text-slate-50 font-sans hover:bg-violet-300 px-10'>Emprendimiento de sahumadores artesanales y más artículos para la renovación de energía.</p>
+            <h2 className="ms-20 mt-20 mb-0 text-slate-50 text-2xl px-5 font-bold">
                 Los más buscados
             </h2>
 
+            
+
             <section className="galeria">
-                <div className="galeria__item"> 
+                <div className="galeria__item ms-10"> 
                     {
                         productos.map((prod) => <ItemCard key={prod.id}  item={prod} />)
                     }
