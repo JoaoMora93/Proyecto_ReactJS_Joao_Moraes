0498300a4b6879b1048c2d61b5eed340df9c118a
Joao Moraes <joaomcarril@gmail.com>
2023-08-11 19:07:45 -0300

    firebase en detail container

--- tienda_reactJS_ JoaoM/src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ tienda_reactJS_ JoaoM/src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react"
 import { pedirDatos } from "../../Helpers/pedirDatos"
 import { useParams } from "react-router-dom"
 import ItemDetail from "../ItemDetail/ItemDetail"
+import { doc, getDoc } from "firebase/firestore"
+import { db } from '../../firebase/config'
+
 
 
 
@@ -16,11 +19,18 @@ const ItemDetailContainer = () => {
     useEffect(() => {
         setLoading(true)
 
-        pedirDatos()
-        .then(res => {
-            setItem( res.find(prod => prod.id === Number(itemId) ))
-        })
-        .finally(() => setLoading(false))
+        //1- armar la ref.
+        const itemRef = doc(db, "productos", itemId)
+        //2- llamar a ref.
+        getDoc(itemRef)
+            .then((doc) => {
+                setItem({
+                    id: doc.id,
+                    ...doc.data()
+                })
+            })
+            .catch(e => console.log(e))
+            .finally(() => setLoading(false))
     }, [])
 
     return (
