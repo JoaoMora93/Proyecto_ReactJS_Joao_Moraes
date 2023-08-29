0cae1a857018076bc5cce99a1acda3d1953dcce2
Joao Moraes <joaomcarril@gmail.com>
2023-08-29 00:19:00 -0300

    chekout2

--- tienda_reactJS_ JoaoM/src/components/Checkout/Checkout.jsx
+++ tienda_reactJS_ JoaoM/src/components/Checkout/Checkout.jsx
@@ -1,10 +1,27 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { CartContext } from "../../context/CartContext"
+import { collection, addDoc } from "firebase/firestore"
+import { db } from "../../firebase/config"
+import { Link } from 'react-router-dom'
 
 
 
 const Checkout = () => {
-    const { cart, totalCompra } = useContext(CartContext)
+    const { cart, totalCompra, vaciarCarrito } = useContext(CartContext)
+
+    const [orderId, setOrderId] = useState(null)
+    const [values, setValues] = useState({
+        nombre: '',
+        direccion:'',
+        email:''
+    })
+
+    const handleInputChange = (e) => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -13,13 +30,33 @@ const Checkout = () => {
 
         const orden = {
             cliente: values,
-            items: cart.map(item => ({id: item.id, precio: item.precio, cantidad: item.cantidad, nombre: item.nombre}))
+            items: cart.map(item => ({id: item.id, precio: item.precio, cantidad: item.cantidad, nombre: item.nombre})),
             total: totalCompra(),
             fhy: new Date()
         }
 
         console.log(orden)
+        // enviarlo a firebase
+        const ordersRef = collection(db, "orders")
 
+        addDoc(ordersRef, orden)
+            .then((doc) => {
+                console.log(doc.id)
+                vaciarCarrito()
+                setOrderId(doc.id)
+            })
+    }
+
+    if (orderId) {
+        return (
+            <div>
+                <h2>Tu compra se realizó con éxito</h2>
+                <hr/>
+                <p>Tu número de orden es: <strong>{orderId}</strong></p>
+
+                <Link to="/">Volver</Link>
+            </div>
+        )
     }
 
     return (
